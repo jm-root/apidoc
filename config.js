21508dd7337b8a7054744a19d7197483446324d1
jammacn <jeff@jamma.cn>
2020-01-18 16:29:52 +0800

    passport

--- config.js
+++ config.js
@@ -8,6 +8,14 @@ var swaggerUrls = [
     name: 'acl 1.0'
   },
   {
+    url: 'specs/passport-2.0.yml',
+    name: 'passport 2.0'
+  },
+  {
+    url: 'specs/passport-mobile-2.0.yml',
+    name: 'passport-mobile 2.0'
+  },
+  {
     url: 'specs/sms-1.0.yml',
     name: 'sms 1.0'
   },
