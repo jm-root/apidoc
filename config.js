8504f62e8e4de4765d7d8e45f23db4f1de2747e0
jeff <jeff@jamma.cn>
2020-05-11 20:34:10 +0800

    add user spec

--- config.js
+++ config.js
@@ -4,10 +4,6 @@ var swaggerUrls = [
     name: 'acl 1.0'
   },
   {
-    url: 'specs/bank-3.2.yml',
-    name: 'bank 3.2'
-  },
-  {
     url: 'specs/sso-1.0.yml',
     name: 'sso 1.0'
   },
@@ -20,11 +16,19 @@ var swaggerUrls = [
     name: 'passport 2.0'
   },
   {
-    url: 'specs/sms-1.0.yml',
-    name: 'sms 1.0'
+    url: 'specs/user-2.1.yml',
+    name: 'user 2.1'
   },
   {
     url: 'specs/userbind-1.0.yml',
     name: 'userbind 1.0'
+  },
+  {
+    url: 'specs/sms-1.0.yml',
+    name: 'sms 1.0'
+  },
+  {
+    url: 'specs/bank-3.2.yml',
+    name: 'bank 3.2'
   },
 ]
